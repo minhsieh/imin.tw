342b75dfb9664c4e1fd3cf07144b9af706821edb
min <side112358@gmail.com>
2016-06-22 20:47:32 +0800

    change email termplate

--- resources/views/mail/comfirm.blade.php
+++ resources/views/mail/comfirm.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>I'm In Studio Comfirm Email</title>
-  <style type="text/css">
-  @import url(http://fonts.googleapis.com/css?family=Droid+Sans);
+  <title>Sunday Confirm Email</title>
+  <!-- Designed by https://github.com/kaytcat -->
+  <!-- Header image designed by Freepik.com -->
 
+
+  <style type="text/css">
   /* Take care of image borders and formatting */
 
   img {
@@ -16,26 +18,21 @@
     -ms-interpolation-mode: bicubic;
   }
 
-  a {
-    text-decoration: none;
-    border: 0;
-    outline: none;
-    color: #bbbbbb;
-  }
+  a img { border: none; }
+  table { border-collapse: collapse !important; }
+  #outlook a { padding:0; }
+  .ReadMsgBody { width: 100%; }
+  .ExternalClass {width:100%;}
+  .backgroundTable {margin:0 auto; padding:0; width:100%;}
+  table td {border-collapse: collapse;}
+  .ExternalClass * {line-height: 115%;}
 
-  a img {
-    border: none;
-  }
 
   /* General styling */
 
-  td, h1, h2, h3  {
-    font-family: Helvetica, Arial, sans-serif ,Microsoft JhengHei;
-    font-weight: 400;
-  }
-
   td {
-    text-align: center;
+    font-family: Arial, sans-serif;
+    color: #6f6f6f;
   }
 
   body {
@@ -43,25 +40,37 @@
     -webkit-text-size-adjust:none;
     width: 100%;
     height: 100%;
-    color: #37302d;
-    background: #ffffff;
-    font-size: 16px;
-    font-family: Microsoft JhengHei;
+    color: #6f6f6f;
+    font-weight: 400;
+    font-size: 18px;
   }
 
-   table {
-    border-collapse: collapse !important;
+
+  h1 {
+    margin: 10px 0;
   }
 
-  .headline {
-    color: #ffffff;
-    font-size: 36px;
+  a {
+    color: #27aa90;
+    text-decoration: none;
   }
 
- .force-full-width {
-  width: 100% !important;
- }
+  .force-full-width {
+    width: 100% !important;
+  }
 
+  .force-width-80 {
+    width: 80% !important;
+  }
+
+
+  .body-padding {
+    padding: 0 75px;
+  }
+
+  .mobile-align {
+    text-align: right;
+  }
 
 
 
@@ -69,10 +78,15 @@
 
   <style type="text/css" media="screen">
       @media screen {
-         /*Thanks Outlook 2013! http://goo.gl/XLxpyl*/
-        td, h1, h2, h3 {
-          font-family: 'Droid Sans', 'Helvetica Neue', 'Arial', 'sans-serif',,Microsoft JhengHei !important;
+        @import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,900);
+        /* Thanks Outlook 2013! */
+        * {
+          font-family: 'Source Sans Pro', 'Helvetica Neue', 'Arial', 'sans-serif',Microsoft JhengHei !important;
         }
+        .w280 {
+          width: 280px !important;
+        }
+
       }
   </style>
 
@@ -80,149 +94,229 @@
     /* Mobile styles */
     @media only screen and (max-width: 480px) {
 
-      table[class="w320"] {
+      table[class*="w320"] {
         width: 320px !important;
       }
 
+      td[class*="w320"] {
+        width: 280px !important;
+        padding-left: 20px !important;
+        padding-right: 20px !important;
+      }
+
+      img[class*="w320"] {
+        width: 250px !important;
+        height: 67px !important;
+      }
+
+      td[class*="mobile-spacing"] {
+        padding-top: 10px !important;
+        padding-bottom: 10px !important;
+      }
+
+      *[class*="mobile-hide"] {
+        display: none !important;
+      }
+
+      *[class*="mobile-br"] {
+        font-size: 12px !important;
+      }
+
+      td[class*="mobile-w20"] {
+        width: 20px !important;
+      }
+
+      img[class*="mobile-w20"] {
+        width: 20px !important;
+      }
+
+      td[class*="mobile-center"] {
+        text-align: center !important;
+      }
+
+      table[class*="w100p"] {
+        width: 100% !important;
+      }
+
+      td[class*="activate-now"] {
+        padding-right: 0 !important;
+        padding-top: 20px !important;
+      }
+
+      td[class*="mobile-block"] {
+        display: block !important;
+      }
+
+      td[class*="mobile-align"] {
+        text-align: left !important;
+      }
 
     }
   </style>
 </head>
-<body class="body" style="padding:0; margin:0; display:block; background:#ffffff; -webkit-text-size-adjust:none" bgcolor="#ffffff">
-<table align="center" cellpadding="0" cellspacing="0" width="100%" height="100%" >
+<body  class="body" style="padding:0; margin:0; display:block; background:#eeebeb; -webkit-text-size-adjust:none;" bgcolor="#eeebeb">
+<table align="center" cellpadding="0" cellspacing="0" width="100%">
   <tr>
-    <td align="center" valign="top" bgcolor="#ffffff"  width="100%">
-      <center>
-        <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" width="600" class="w320">
-          <tr>
-            <td align="center" valign="top">
+    <td align="center" valign="top" style="background-color:#eeebeb" width="100%">
 
-                <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" width="100%" style="margin:0 auto;">
+    <center>
+
+      <table cellspacing="0" cellpadding="0" width="600" class="w320">
+        <tr>
+          <td align="center" valign="top">
+
+
+          <table style="margin:0 auto;" cellspacing="0" cellpadding="0" width="100%">
+            <tr>
+              <td style="text-align: center;">
+                <a href="#"><img class="w320" height="120" src="{{asset('assets/img/mail/logo.png')}}" alt="company logo" /></a>
+              </td>
+            </tr>
+          </table>
+
+
+          <table cellspacing="0" cellpadding="0" class="force-full-width" style="background-color:#3bcdb0;">
+            <tr>
+              <td style="background-color:#3bcdb0;">
+
+                <table cellspacing="0" cellpadding="0" class="force-full-width">
                   <tr>
-                    <td style="font-size: 30px; text-align:center;">
-                      <br>
-                        感謝您的聯絡
-                      <br>
-                      <br>
+                    <td style="font-size:40px; font-weight: 600; color: #ffffff; text-align:center;" class="mobile-spacing">
+                    <div class="mobile-br">&nbsp;</div>
+                      親愛的{{$name}}，您的訊息我們收到了
+                    <br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="font-size:24px; text-align:center; padding: 0 75px; color: #6f6f6f;" class="w320 mobile-spacing">
+                     我們將會盡快回覆您，謝謝
                     </td>
                   </tr>
                 </table>
 
-                <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" width="100%" bgcolor="#4dbfbf">
+                <table cellspacing="0" cellpadding="0" width="100%">
                   <tr>
                     <td>
-                    <br>
-                      <img src="{{asset('assets/img/mail/logo.png')}}" width="200" alt="imin-logo">
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="headline">
-                      親愛的 {{$name}}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-
-                      <center>
-                        <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" width="60%">
-                          <tr>
-                            <td style="color:#187272;">
-                            <br>
-                             我們已經收到您的訊息了，會盡速與您聯絡喔！
-                            <br>
-                            <br>
-                            <br>
-                            </td>
-                          </tr>
-                        </table>
-                      </center>
-
+                      <img src="https://www.filepicker.io/api/file/4BgENLefRVCrgMGTAENj" style="max-width:100%; display:block;">
                     </td>
                   </tr>
                 </table>
 
-                <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" width="100%" bgcolor="#f5774e">
+              </td>
+            </tr>
+          </table>
+
+          <table cellspacing="0" cellpadding="0" class="force-full-width" bgcolor="#ffffff" >
+            <tr>
+              <td style="background-color:#ffffff; padding-top: 15px;">
+
+              <center>
+                <table style="margin:0 auto;" cellspacing="0" cellpadding="0" class="force-width-80">
                   <tr>
-                    <td>
+                    <td style="text-align:left;">
                     <br>
-                      <img src="https://www.filepicker.io/api/file/hkpp4OzbQme8bszfOs1k" width="113" height="100" alt="meter image">
+                    <strong>訊息內容:</strong><br>
+                      {!!$content!!}
                     </td>
-                  </tr>
-                  <tr>
-                    <td>
-
-                      <center>
-                        <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" width="60%">
-                          <tr>
-                            <td style="color:#933f24;">
-                              {{$content}}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td style="color:#933f24;">
-                              {{$email}}
-                              <br><br>
-                            </td>
-                          </tr>
-                        </table>
-                      </center>
-
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div><!--[if mso]>
-                        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://" style="height:50px;v-text-anchor:middle;width:200px;" arcsize="8%" stroke="f" fillcolor="#ac4d2f">
-                          <w:anchorlock/>
-                          <center>
-                        <![endif]-->
-                            <a href="http://"
-                      style="background-color:#ac4d2f;border-radius:4px;color:#ffffff;display:inline-block;font-family: Helvetica, Arial, sans-serif;font-size:16px;font-weight:bold;line-height:50px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;">My Account</a>
-                        <!--[if mso]>
-                          </center>
-                        </v:roundrect>
-                      <![endif]--></div>
-                      <br>
-                      <br>
+                    <td style="text-align:right; vertical-align:top;">
+                    <br>
+                    <b>Email: </b> <br>
+                    {{$email}}
                     </td>
                   </tr>
                 </table>
 
-                <table style="margin: 0 auto;" cellpadding="0" cellspacing="0" class="force-full-width" bgcolor="#414141" style="margin: 0 auto">
+                <table style="margin:0 auto;" cellspacing="0" cellpadding="0" class="force-width-80">
                   <tr>
-                    <td style="background-color:#414141;">
+                    <td  class="mobile-block">
                     <br>
-                    <br>
-                      <img src="https://www.filepicker.io/api/file/R4VBTe2UQeGdAlM7KDc4" alt="google+">
-                      <img src="https://www.filepicker.io/api/file/cvmSPOdlRaWQZnKFnBGt" alt="facebook">
-                      <img src="https://www.filepicker.io/api/file/Gvu32apSQDqLMb40pvYe" alt="twitter">
-                      <br>
-                      <br>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color:#bbbbbb; font-size:12px;">
-                      <a href="#">View in browser</a> | <a href="#">Unsubscribe</a> | <a href="#">Contact</a>
-                      <br><br>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style="color:#bbbbbb; font-size:12px;">
-                       © 2014 All Rights Reserved
-                       <br>
-                       <br>
+
+                      <table cellspacing="0" cellpadding="0" class="force-full-width">
+                        <tr>
+                          <td style="border-bottom:1px solid #e3e3e3; font-weight: bold; text-align:left">
+                          
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="text-align:left;">
+                           
+                          </td>
+                        </tr>
+                      </table>
                     </td>
                   </tr>
                 </table>
 
 
+                <table style="margin: 0 auto;" cellspacing="0" cellpadding="0" class="force-width-80">
+                  <tr>
+                    <td style="text-align: left;">
+                    <br>
+                      
+                    </td>
+                  </tr>
+                </table>
+              </center>
+
+              <table style="margin:0 auto;" cellspacing="0" cellpadding="10" width="100%">
+                <tr>
+                  <td style="text-align:center; margin:0 auto;">
+                  <br>
+                    
+                    <br>
+                  </td>
+                </tr>
+              </table>
+
+
+              <table cellspacing="0" cellpadding="0" bgcolor="#363636"  class="force-full-width">
+                <tr>
+                  <td style="background-color:#363636; text-align:center;">
+                  <br>
+                  <br>
+                    <img width="62" height="56" img src="https://www.filepicker.io/api/file/FjkhDKXsTFyaHnXhhBCw">
+                    <img width="68" height="56" src="https://www.filepicker.io/api/file/W6gXqm5BRL6qSvQRcI7u">
+                    <img width="61" height="56" src="https://www.filepicker.io/api/file/eV9YfQkBTiaOu9PA9gxv">
+                  <br>
+                  <br>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="color:#f0f0f0; font-size: 14px; text-align:center; padding-bottom:4px;">
+                    © 2014 All Rights Reserved
+                  </td>
+                </tr>
+                <tr>
+                  <td style="color:#27aa90; font-size: 14px; text-align:center;">
+                    <a href="#">View in browser</a> | <a href="#">Contact</a> | <a href="#">Unsubscribe</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="font-size:12px;">
+                    &nbsp;
+                  </td>
+                </tr>
+              </table>
+
+              </td>
+            </tr>
+          </table>
 
 
 
-            </td>
-          </tr>
-        </table>
+
+
+
+
+          </td>
+        </tr>
+      </table>
+
     </center>
+
+
+
+
     </td>
   </tr>
 </table>
